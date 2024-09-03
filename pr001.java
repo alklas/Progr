8a39968e39387102f3509add874a45c07083166d
alklas <alklas@mail.ru>
2024-09-03 14:11:31 +0700

    Добавлены переменные и примеры деления

--- pr001.java
+++ pr001.java
@@ -1,5 +1,14 @@
 class pr001 {
 	public static void main(String args[]) {
 		System.out.println("Первая программа на Java");
+		//Выводим строку на консоль
+		int Var1 = 1024;
+		int Var2345678;
+		//System.out.println(Var2345678); Ошибка: переменной не присвоено значение
+		Var2345678 = 411;
+		System.out.println(Var1/2);
+		System.out.println("Результат целочисленного деления: " + Var2345678/2);
+		System.out.println("Результат деления приведением типа данных: " + (double)Var2345678/2);
+		double Var3 = (double)Var2345678/2;
 	}
 }
