1235347358efd35788e406a57fc5a6cd4fa73438
alklas <alklas@mail.ru>
2024-11-19 13:37:39 +0700

    Добавили подкласс Грузовики в программу pr004

--- pr004.java
+++ pr004.java
@@ -1,7 +1,7 @@
 class Avtomobil {
-	int pass; //количество пассажиров
-	int v; //объём топливного бака в л.
-	double rash; //расход топлива в л. на 100 км.
+	private int pass; //количество пассажиров
+	private int v; //объём топливного бака в л.
+	private double rash; //расход топлива в л. на 100 км.
 	
 	Avtomobil(int p, int V, double r) {
 		pass = p;
@@ -21,12 +21,50 @@ class Avtomobil {
 		return ((double)km/100)*rash;
 	}
 
+	//Методы доступа к переменным экземпляра 
+	int getPass() {
+		return pass;
+	}
+	void setPass(int p) {
+		pass = p;
+	}
+	int getV() {
+		return v;
+	}
+	void setV(int V) {
+		v = V;
+	}
+	double getRash() {
+		return rash;
+	}
+	void setRash(double r) {
+		rash = r;
+	}
+
+}
+class Truck extends Avtomobil {
+	private int cargocap; //грузоподъёмность
+	
+	//Конструктор
+	Truck(int p, int V, double r, int c) {
+		super(p, V, r);
+		cargocap = c;
+	}
+
+	int getCargo() {
+		return cargocap;
+	}
+	void putCargo(int c) {
+		cargocap = c;
+	}
 }
 class pr004 {
 	public static void main(String args[]) {
 		//Создание экземпляра класса
 		Avtomobil lada = new Avtomobil(4, 40, 5.5);
 		Avtomobil porshe = new Avtomobil(1, 100, 14.0);
+		Truck gazelnext = new Truck(2, 100, 25.5, 3500);
+		Truck ural = new Truck(3, 200, 35.1, 5000);
 		int rasst, rasst2;
 
 		//Присваивание значений внутренним переменным экземпляра класса
@@ -40,9 +78,13 @@ class pr004 {
 		porshe.rash = 14.0;
 		*/
 
-		System.out.println("Для расстояния в 10 км. нужно " + lada.fuelNeeded(10) + " литров бензина");
+		System.out.println("Для расстояния в 10 км. ладе нужно " + lada.fuelNeeded(10) + " литров бензина");
+		System.out.println("Для расстояния в 10 км. уралу нужно " + ural.fuelNeeded(10) + " литров бензина");
+		System.out.println("Урал перевезёт " + ural.getCargo() + " кг. грузов");
+		System.out.println("Для расстояния в 10 км. газели нужно " + gazelnext.fuelNeeded(10) + " литров бензина");
 		rasst=10;
-		System.out.println("Для расстояния в 10 км. нужно " + lada.fuelNeeded(rasst) + " литров бензина");
+		System.out.println("Для расстояния в 10 км. порше нужно " + porshe.fuelNeeded(rasst) + " литров бензина");
+	
 
 		/*
 		Avtomobil mercedes;	
