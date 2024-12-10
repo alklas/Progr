6902f4e1dbb8dd1405e952624c37d23eb9e9c773
alklas <alklas@mail.ru>
2024-12-10 11:27:05 +0700

    Демонстрация интерфейса Series и его реализации ByTwos

--- bookpackext/pr011.java
+++ bookpackext/pr011.java
@@ -1,12 +1,19 @@
 package bookpackext;
+
+//Импорт класса из пакета bookpack
+import bookpack.Book;
+/*
+ * Импорт всех классов из пакета bookpack
+ * import bookpack.*;
+ */
 class pr011 {
 	public static void main(String[] args){
-		bookpack.Book[] books = new bookpack.Book[5];
-                books[0] = new bookpack.Book("Иллиада", "Гомер", 1980);
-                books[1] = new bookpack.Book("Унесённые ветром", "Митчел", 2000);
-                books[2] = new bookpack.Book("Наедине с собой", "Аврелий", 1950);
-                books[3] = new bookpack.Book("Собор Паржиской богоматери", "Гюго", 1970);
-                books[4] = new bookpack.Book("Преступление и наказание", "Достоевский", 1950);
+		Book[] books = new Book[5];
+                books[0] = new Book("Иллиада", "Гомер", 1980);
+                books[1] = new Book("Унесённые ветром", "Митчел", 2000);
+                books[2] = new Book("Наедине с собой", "Аврелий", 1950);
+                books[3] = new Book("Собор Паржиской богоматери", "Гюго", 1970);
+                books[4] = new Book("Преступление и наказание", "Достоевский", 1950);
                 for(int i=0; i < books.length; i++)
                         books[i].show();
         }
